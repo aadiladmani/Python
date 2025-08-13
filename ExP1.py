bde4d8847b393fef939ad617435bb2106731627d
aadiladmani <mohdaadiladmani.136995@marwadiuniversity.ac.in>
2025-08-13 14:11:03 +0530

    Add files via upload

---
+++ ExP1.py
@@ -0,0 +1,4 @@
+tup = (1, 2, 3, 2, 4, 2, 5)
+element = 2
+count = tup.count(element)
+print(f"Occurrences of {element}: {count}")
---
+++ ExP2.py
@@ -0,0 +1,7 @@
+tup = (10, 20, 30, 40)
+element = 30
+
+if element in tup:
+    print(f"{element} exists in the tuple")
+else:
+    print(f"{element} does not exist in the tuple")
